35b6c9ef7aa6624d60c11d8e59bab248a21bb8bb
vboxuser <vboxuser@user.myguest.virtualbox.org>
2025-04-03 12:20:13 +0300

    add emoji buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import EmojiButton from './EmojiButton';
 
 function App() {
   return (
-    <div >
-      This is the frontend
+    <div>
+      <div>
+        This is the fronend
+      </div>
+      <div >
+        <EmojiButton symbol="❤️" label="heart" />
+        <EmojiButton symbol="👍" label="like" />
+        <EmojiButton symbol="😢" label="cry" />
+        <EmojiButton symbol="😡" label="angry" />
+      </div >
     </div>
   );
 }
