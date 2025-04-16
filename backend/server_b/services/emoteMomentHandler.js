7c2a70a2282deda1256bc266dd044c1833d871a0
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2025-04-16 10:59:21 +0300

    Include meaningful moment as part of kafka message

--- backend/server_b/services/emoteMomentHandler.js
+++ backend/server_b/services/emoteMomentHandler.js
@@ -9,10 +9,30 @@ const kafka = new Kafka({
   })
 
 const consumerTopic = 'raw-emote-data';
-
 const consumer = kafka.consumer({ groupId: 'server_b_group' });
 
+const admin = kafka.admin();
+const producer = kafka.producer();
+const producerTopic = 'aggregated-emote-data';
+
 const { threshold } = getConfig();
+
+const sendMeaningfulMoments = async (moments) => {
+    await admin.connect();
+    await admin.createTopics({
+        topics: [
+            { topic: producerTopic, numPartitions: 1, replicationFactor: 1 }
+        ]
+    });
+    await producer.connect();
+    await producer.send({
+        topic: producerTopic,
+        messages: [{ value: JSON.stringify(moments) }]
+    });
+    await producer.disconnect();
+    await admin.disconnect();
+    console.log('Meaningful moments sent to producer:', moments);
+ };
 
 const consumRawEmotes = async () => {
     await consumer.connect()
@@ -25,15 +45,21 @@ const consumRawEmotes = async () => {
           emoteCollection.push(JSON.parse(message.value.toString()));
 
         }else{
-            console.log(analyzeEmotes(emoteCollection, threshold));
-            console.log(JSON.stringify(emoteCollection, null, 2));
-            emoteCollection.length = 0;
+            const moments = analyzeEmotes(emoteCollection, threshold);
+            if(moments.length > 0) {
+                await sendMeaningfulMoments(moments);
+                emoteCollection.length = 0;
+            }else{
+                emoteCollection.shift();
+            }
+
             emoteCollection.push(JSON.parse(message.value.toString()));
         }
       },
     })
 };
 
-consumRawEmotes().catch("##### from moment",console.error);
+consumRawEmotes().catch("consume emotes error",console.error);
+
 
 
