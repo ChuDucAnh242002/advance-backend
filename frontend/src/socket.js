1d27ee1b90f639188121773638baf2697a66cf6f
vboxuser <vboxuser@user.myguest.virtualbox.org>
2025-04-17 10:51:08 +0300

    fetch aggregated emotes data from server_b

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,5 +3,9 @@ import { io } from "socket.io-client"
 const URL = "http://localhost:3001"
 
 export const socket = io(URL, {
-    transports: ["websocket", "polling"]
+    transports: ["websocket", "polling"],
+    autoConnect: false,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000
 });
