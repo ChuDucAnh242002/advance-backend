e18f91d45a99e601e3e74ae8d94c7f68b1c297c9
ChuDucAnh242002 <anh.chu@tuni.fi>
2025-04-28 19:54:50 +0300

    Add comment to emotegen

--- backend/emotegen/index.js
+++ backend/emotegen/index.js
@@ -34,8 +34,6 @@ const sendMessage = async (message) => {
         topic,
         messages: [{ value: JSON.stringify(message) }]
     });
-
-    //console.log('Message sent: ', message);
 }
 
 const generateEmotes = async () => {
@@ -43,8 +41,9 @@ const generateEmotes = async () => {
     await producer.connect();
 
     let interval = 1000;
+    // Get the interval from server_b
     try {
-        const response = await axios.get('http://server_b:8000/settings/interval', 
+        const response = await axios.get('http://server_b:8000/settings/interval',
             { headers: { 'Content-Type': 'application/json' } }
         );
         if (response.status === 200 && response.data.interval) {
@@ -57,7 +56,8 @@ const generateEmotes = async () => {
     }
 
     setInterval(async () => {
-        const emotes = await axios.get('http://server_b:8000/settings/allowed-emotes', 
+        // Get the allowed-emotes from server_b
+        const emotes = await axios.get('http://server_b:8000/settings/allowed-emotes',
             { headers: { 'Content-Type': 'application/json' } }
         ).then(response => {
             if (response.status !== 200) {
@@ -75,14 +75,15 @@ const generateEmotes = async () => {
             return;
         }
 
-        if(Math.random() < 0.2) {
+        // Send random emotes as a producer to Kafka
+        if (Math.random() < 0.2) {
             const burstEmote = emotes[Math.floor(Math.random() * emotes.length)];
-            for(let i = 0; i < Math.floor(Math.random() * 51) + 50; i++) {
+            for (let i = 0; i < Math.floor(Math.random() * 51) + 50; i++) {
                 const message = { emote: burstEmote, timestamp: new Date().toISOString() };
                 await sendMessage(message);
             }
         } else {
-            for(let i = 0; i < Math.floor(Math.random() * 16) + 5; i++) {
+            for (let i = 0; i < Math.floor(Math.random() * 16) + 5; i++) {
                 const message = await getRandomEmote(emotes);
                 await sendMessage(message);
             }
